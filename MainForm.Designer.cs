8563d506b7ac8c52df6155e5dbd50f8ea66e83fc
shunf4 <shun1048576@gmail.com>
2022-07-23 21:11:57 +0800

    allow adding gateway and arp entry, making a network identified

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,7 +28,21 @@
             this.NameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.DescriptionColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Category = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnAddNetworkProfile = new System.Windows.Forms.Button();
+            this.cmbNetworkAdapters = new System.Windows.Forms.ComboBox();
+            this.txbFakeIp = new System.Windows.Forms.TextBox();
+            this.lblNetworkAdapters = new System.Windows.Forms.Label();
+            this.lblFakeIp = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSetNdisDeviceType = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.NetworksGrid)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // NetworksGrid
@@ -43,11 +57,11 @@
             this.DescriptionColumn,
             this.Category});
             this.NetworksGrid.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.NetworksGrid.Location = new System.Drawing.Point(0, 0);
+            this.NetworksGrid.Location = new System.Drawing.Point(0, 102);
             this.NetworksGrid.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.NetworksGrid.Name = "NetworksGrid";
             this.NetworksGrid.RowHeadersWidth = 51;
-            this.NetworksGrid.Size = new System.Drawing.Size(1072, 301);
+            this.NetworksGrid.Size = new System.Drawing.Size(1130, 326);
             this.NetworksGrid.TabIndex = 0;
             this.NetworksGrid.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.NetworksGrid_CellValueChanged);
             this.NetworksGrid.UserDeletingRow += new System.Windows.Forms.DataGridViewRowCancelEventHandler(this.NetworksGrid_UserDeletingRow);
@@ -87,16 +101,126 @@
             this.Category.ToolTipText = "Category indicates whether this network is public or private, so different securi" +
     "ty policies can apply.";
             // 
+            // btnAddNetworkProfile
+            // 
+            this.btnAddNetworkProfile.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAddNetworkProfile.Location = new System.Drawing.Point(15, 15);
+            this.btnAddNetworkProfile.Name = "btnAddNetworkProfile";
+            this.btnAddNetworkProfile.Size = new System.Drawing.Size(229, 28);
+            this.btnAddNetworkProfile.TabIndex = 1;
+            this.btnAddNetworkProfile.Text = "Add Network Profile";
+            this.btnAddNetworkProfile.UseVisualStyleBackColor = true;
+            this.btnAddNetworkProfile.Click += new System.EventHandler(this.btnAddNetworkProfile_Click);
+            // 
+            // cmbNetworkAdapters
+            // 
+            this.cmbNetworkAdapters.Dock = System.Windows.Forms.DockStyle.Top;
+            this.cmbNetworkAdapters.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbNetworkAdapters.FormattingEnabled = true;
+            this.cmbNetworkAdapters.Location = new System.Drawing.Point(20, 20);
+            this.cmbNetworkAdapters.Name = "cmbNetworkAdapters";
+            this.cmbNetworkAdapters.Size = new System.Drawing.Size(621, 23);
+            this.cmbNetworkAdapters.TabIndex = 2;
+            // 
+            // txbFakeIp
+            // 
+            this.txbFakeIp.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.txbFakeIp.Location = new System.Drawing.Point(20, 57);
+            this.txbFakeIp.Name = "txbFakeIp";
+            this.txbFakeIp.Size = new System.Drawing.Size(621, 25);
+            this.txbFakeIp.TabIndex = 3;
+            this.txbFakeIp.Text = "25.255.255.254";
+            // 
+            // lblNetworkAdapters
+            // 
+            this.lblNetworkAdapters.AutoSize = true;
+            this.lblNetworkAdapters.Location = new System.Drawing.Point(30, 23);
+            this.lblNetworkAdapters.Name = "lblNetworkAdapters";
+            this.lblNetworkAdapters.Size = new System.Drawing.Size(143, 15);
+            this.lblNetworkAdapters.TabIndex = 4;
+            this.lblNetworkAdapters.Text = "Choose an Adapter";
+            // 
+            // lblFakeIp
+            // 
+            this.lblFakeIp.AutoSize = true;
+            this.lblFakeIp.Location = new System.Drawing.Point(46, 60);
+            this.lblFakeIp.Name = "lblFakeIp";
+            this.lblFakeIp.Size = new System.Drawing.Size(127, 15);
+            this.lblFakeIp.TabIndex = 5;
+            this.lblFakeIp.Text = "Fake Gateway IP";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnSetNdisDeviceType);
+            this.panel1.Controls.Add(this.btnAddNetworkProfile);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(871, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(15);
+            this.panel1.Size = new System.Drawing.Size(259, 102);
+            this.panel1.TabIndex = 6;
+            // 
+            // btnSetNdisDeviceType
+            // 
+            this.btnSetNdisDeviceType.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnSetNdisDeviceType.Location = new System.Drawing.Point(15, 57);
+            this.btnSetNdisDeviceType.Name = "btnSetNdisDeviceType";
+            this.btnSetNdisDeviceType.Size = new System.Drawing.Size(229, 30);
+            this.btnSetNdisDeviceType.TabIndex = 2;
+            this.btnSetNdisDeviceType.Text = "Set *NdisDeviceType=1";
+            this.btnSetNdisDeviceType.UseVisualStyleBackColor = true;
+            this.btnSetNdisDeviceType.Click += new System.EventHandler(this.btnSetNdisDeviceType_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1130, 102);
+            this.panel2.TabIndex = 7;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.cmbNetworkAdapters);
+            this.panel4.Controls.Add(this.txbFakeIp);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(210, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Padding = new System.Windows.Forms.Padding(20);
+            this.panel4.Size = new System.Drawing.Size(661, 102);
+            this.panel4.TabIndex = 7;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.lblNetworkAdapters);
+            this.panel3.Controls.Add(this.lblFakeIp);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(210, 102);
+            this.panel3.TabIndex = 6;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1072, 301);
+            this.ClientSize = new System.Drawing.Size(1130, 428);
             this.Controls.Add(this.NetworksGrid);
+            this.Controls.Add(this.panel2);
             this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "MainForm";
             this.Text = "Network Rename Tool";
             ((System.ComponentModel.ISupportInitialize)(this.NetworksGrid)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -108,6 +232,16 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn NameColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn DescriptionColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn Category;
+        private System.Windows.Forms.Button btnAddNetworkProfile;
+        private System.Windows.Forms.ComboBox cmbNetworkAdapters;
+        private System.Windows.Forms.TextBox txbFakeIp;
+        private System.Windows.Forms.Label lblNetworkAdapters;
+        private System.Windows.Forms.Label lblFakeIp;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnSetNdisDeviceType;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -1,5 +1,12 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
 using Microsoft.Win32;
+using static Vanara.PInvoke.IpHlpApi;
 
 namespace NetworkRenameTool {
 
@@ -14,6 +21,7 @@ namespace NetworkRenameTool {
         public MainForm() {
             InitializeComponent();
             FeedProfiles();
+            FeedNetworkAdapters();
         }
 
         /// <summary>
@@ -27,6 +35,18 @@ namespace NetworkRenameTool {
                         NetworksGrid.Rows.Add(new[] { subkeyName, subkey.GetValue(ProfileNameKey), subkey.GetValue(DescriptionKey), System.Convert.ToInt32((subkey.GetValue(CategoryKey))) });
                     }
                 }
+            }
+        }
+
+        private void FeedNetworkAdapters()
+        {
+            IEnumerable<IP_ADAPTER_ADDRESSES> adapterAddresses_es = GetAdaptersAddresses();
+            cmbNetworkAdapters.DataSource = Enumerable.ToList(adapterAddresses_es.Select(a => new KeyValuePair<string, NET_LUID>(a.FriendlyName + " " + a.AdapterName + " " + $"0x{a.Luid.Value:X}", a.Luid)).ToList());
+            cmbNetworkAdapters.ValueMember = "Value";
+            cmbNetworkAdapters.DisplayMember = "Key";
+            if (cmbNetworkAdapters.Items.Count > 0)
+            {
+                cmbNetworkAdapters.SelectedIndex = 0;
             }
         }
 
@@ -95,6 +115,140 @@ namespace NetworkRenameTool {
 
         #endregion
 
+        private bool searchSetNetworkAdapterClassNdisDeviceType(string targetNetCfgInstanceId, uint val)
+        {
+            bool nwAdapterClassFound = false;
+            using (var nwAdapterClasses = Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}", true))
+            {
+                foreach (var classIndex in nwAdapterClasses.GetSubKeyNames())
+                {
+                    using (var classSubKey = nwAdapterClasses.OpenSubKey(classIndex, true))
+                    {
+                        var netCfgInstanceIdRaw = classSubKey.GetValue("NetCfgInstanceId");
+                        if (netCfgInstanceIdRaw != null && netCfgInstanceIdRaw is string)
+                        {
+                            var netCfgInstanceId = (string)netCfgInstanceIdRaw;
+                            if (netCfgInstanceId.ToLower().Equals(targetNetCfgInstanceId.ToLower()))
+                            {
+                                classSubKey.SetValue("*NdisDeviceType", val, RegistryValueKind.DWord);
+                                nwAdapterClassFound = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            return nwAdapterClassFound;
+        }
+
+        private void btnAddNetworkProfile_Click(object sender, System.EventArgs e)
+        {
+            var luid = (NET_LUID)cmbNetworkAdapters.SelectedValue;
+            var ifInfo = GetAdaptersAddresses().Where(x => x.Luid.Equals(luid)).First();
+
+            var unicastAddressList = ifInfo.UnicastAddresses.Where(ip => ip.Address.GetSOCKADDR().si_family == Vanara.PInvoke.Ws2_32.ADDRESS_FAMILY.AF_INET && !ip.Address.GetSOCKADDR().Ipv4.ToString().StartsWith("169.254")).Take(1).ToList();
+
+            if (unicastAddressList.Count == 0)
+            {
+                MessageBox.Show("Seems no IPv4 address currently configured for this adapter. Please set one first!");
+                return;
+            }
+
+            var ipv4Address = unicastAddressList[0].Address.GetSOCKADDR().Ipv4;
+
+            var fakeIp = System.Net.IPAddress.Parse(txbFakeIp.Text);
+            var ipBytes = fakeIp.GetAddressBytes();
+            var fakeIpRaw = (uint)ipBytes[3] << 24;
+            fakeIpRaw += (uint)ipBytes[2] << 16;
+            fakeIpRaw += (uint)ipBytes[1] << 8;
+            fakeIpRaw += (uint)ipBytes[0];
+
+            Vanara.PInvoke.Win32Error result = Vanara.PInvoke.Win32Error.NO_ERROR;
+
+            var luidHash = new System.Security.Cryptography.MD5CryptoServiceProvider().ComputeHash(System.BitConverter.GetBytes(ifInfo.Luid.Value)).Take(3).ToArray();
+
+            var ipnrPhysicalAddressStr = "";
+
+            int i;
+            for (i = 0; i < 8; ++i)
+            {
+                MIB_IPNET_ROW2 ipnr = default;
+                ipnr.Address.si_family = Vanara.PInvoke.Ws2_32.ADDRESS_FAMILY.AF_INET;
+                ipnr.Address.Ipv4.sin_addr.S_addr = fakeIpRaw;
+                ipnr.InterfaceLuid.Value = ifInfo.Luid.Value;
+                ipnr.PhysicalAddress = new byte[IF_MAX_PHYS_ADDRESS_LENGTH];
+                ipnr.PhysicalAddress[0] = 0x00; // just make something up that's consistent and not part of this net
+                ipnr.PhysicalAddress[1] = 0x00;
+                ipnr.PhysicalAddress[2] = 0xA3;
+                ipnr.PhysicalAddress[3] = luidHash[0];
+                ipnr.PhysicalAddress[4] = luidHash[1];
+                ipnr.PhysicalAddress[5] = luidHash[2];
+                ipnrPhysicalAddressStr = System.BitConverter.ToString(ipnr.PhysicalAddress.Take(6).ToArray());
+                ipnr.PhysicalAddressLength = 6;
+                ipnr.State = NL_NEIGHBOR_STATE.NlnsPermanent;
+                ipnr.Flags = MIB_IPNET_ROW2_FLAGS.IsRouther;
+                ipnr.ReachabilityTime = 0; // LastReachable
+                result = CreateIpNetEntry2(ref ipnr);
+                if (result != Vanara.PInvoke.Win32Error.NO_ERROR)
+                    Thread.Sleep(250);
+                else break;
+            }
+            if (i == 8)
+            {
+                MessageBox.Show("CreateIpNetEntry2 Failure: " + result.ToString());
+                return;
+            }
+
+            for (i = 0; i < 8; ++i)
+            {
+                MIB_IPFORWARD_ROW2 nr;
+                InitializeIpForwardEntry(out nr);
+                nr.InterfaceLuid.Value = ifInfo.Luid.Value;
+                nr.DestinationPrefix.Prefix.si_family = Vanara.PInvoke.Ws2_32.ADDRESS_FAMILY.AF_INET; // rest is left as 0.0.0.0/0
+                nr.NextHop.si_family = Vanara.PInvoke.Ws2_32.ADDRESS_FAMILY.AF_INET;
+                nr.NextHop.Ipv4.sin_addr.S_addr = fakeIpRaw;
+                nr.Metric = 9999; // do not use as real default route
+                nr.Protocol = MIB_IPFORWARD_PROTO.MIB_IPPROTO_NETMGMT;
+
+                result = CreateIpForwardEntry2(ref nr);
+                if (result != Vanara.PInvoke.Win32Error.NO_ERROR)
+                    Thread.Sleep(250);
+                else break;
+            }
+            if (i == 8)
+            {
+                MessageBox.Show("CreateIpForwardEntry2 Failure: " + result.ToString());
+                return;
+            }
+
+            bool nwAdapterClassFound;
+            nwAdapterClassFound = searchSetNetworkAdapterClassNdisDeviceType(ifInfo.AdapterName, 0);
+
+            if (nwAdapterClassFound == false)
+            {
+                MessageBox.Show($@"NetCfgInstanceId={ifInfo.AdapterName} not found in HKLM\SYSTEM\CurrentControlSet\Control\Class");
+                return;
+            }
+
+            MessageBox.Show($"Successfully added ARP entry {fakeIp} ~ {ipnrPhysicalAddressStr} (On {ipv4Address}), and created default route 0.0.0.0/0 ~ {fakeIp}.\nYou may need to reboot your PC, set an IP address to this adapter and execute this operation again to assign a network profile.");
+        }
+
+        private void btnSetNdisDeviceType_Click(object sender, EventArgs e)
+        {
+            var luid = (NET_LUID)cmbNetworkAdapters.SelectedValue;
+            var ifInfo = GetAdaptersAddresses().Where(x => x.Luid.Equals(luid)).First();
+
+            bool nwAdapterClassFound;
+            nwAdapterClassFound = searchSetNetworkAdapterClassNdisDeviceType(ifInfo.AdapterName, 1);
+
+            if (nwAdapterClassFound == false)
+            {
+                MessageBox.Show($@"NetCfgInstanceId={ifInfo.AdapterName} not found in HKLM\SYSTEM\CurrentControlSet\Control\Class");
+                return;
+            }
+
+            MessageBox.Show("Success. Please reboot your PC.");
+        }
     }
 
 }
